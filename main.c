3f456f94ce46246682cd5a80f54fac409e7e5da8
vicente944 <vicenteluiz07@gmail.com>
2026-07-21 17:57:25 -0300

    Primeiro commit, iniciando com testes

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main () {
+    FILE *clientes;
+    char carac;
+    clientes = fopen("clientes.csv", "w"); // Mudar de w para outro tipo de abertura posteriormente
+
+    if(clientes == NULL) {
+    printf("Nao foi possivel abrir o arquivo.");
+    exit(1);
+    }
+
+    fprintf(clientes, "cliente,telefone,email");
+
+    fclose(clientes);
+}
